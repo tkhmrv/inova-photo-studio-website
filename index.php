71056074cb798113d1a720b2b6daedea297c9457
Semyon <semyon.tkhmrv@gmail.com>
2024-12-14 12:15:20 +0300

    checkpoint 3 - before php

--- index.php
+++ index.php
@@ -427,50 +427,50 @@
 			<div class="row">
 				<div class="col-lg-4 col-md-6 mb-4">
 					<div class="post-entry-1 h-100">
-						<a href="single.php">
+						<a href="news-film.php">
 							<img src="images/img_3.jpg" alt="Image" class="img-fluid">
 						</a>
 						<div class="post-entry-1-contents">
 							<span class="meta d-inline-block mb-0">July 17, 2019 <span class="mx-2">by</span> <a
 									href="#">Admin</a></span>
-							<h2 class="mb-3"><a href="single.php">How Awesome Stay connected</a></h2>
+							<h2 class="mb-3"><a href="news-film.php">How Awesome Stay connected</a></h2>
 
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and
 								Consonantia, there live the blind texts.</p>
-							<p><a href="single.php">Read more</a></p>
+							<p><a href="news-film.php">Read more</a></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6 mb-4">
 					<div class="post-entry-1 h-100">
-						<a href="single.php">
+						<a href="news-film.php">
 							<img src="images/img_4.jpg" alt="Image" class="img-fluid">
 						</a>
 						<div class="post-entry-1-contents">
 
 							<span class="meta d-inline-block mb-0">July 17, 2019 <span class="mx-2">by</span> <a
 									href="#">Admin</a></span>
-							<h2 class="mb-3"><a href="single.php">We Need Unlimitted People</a></h2>
+							<h2 class="mb-3"><a href="news-film.php">We Need Unlimitted People</a></h2>
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and
 								Consonantia, there live the blind texts.</p>
-							<p><a href="single.php">Read more</a></p>
+							<p><a href="news-film.php">Read more</a></p>
 						</div>
 					</div>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
 					<div class="post-entry-1 h-100">
-						<a href="single.php">
+						<a href="news-film.php">
 							<img src="images/img_5.jpg" alt="Image" class="img-fluid">
 						</a>
 						<div class="post-entry-1-contents">
 
 							<span class="meta d-inline-block mb-0">July 17, 2019 <span class="mx-2">by</span> <a
 									href="#">Admin</a></span>
-							<h2 class="mb-3"><a href="single.php">Important of getting a notifications</a></h2>
+							<h2 class="mb-3"><a href="news-film.php">Important of getting a notifications</a></h2>
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and
 								Consonantia, there live the blind texts.</p>
-							<p><a href="single.php">Read more</a></p>
+							<p><a href="news-film.php">Read more</a></p>
 						</div>
 					</div>
 				</div>
